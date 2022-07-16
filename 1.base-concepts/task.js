8e5f0ad2bc1309a2eb4e5d6cfa007cfaca4e3c94
Yuliya Smirnova <lavruhinayb@gmail.com>
2022-07-16 13:33:02 +0300

    first task solved

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,22 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  arr = []
+  let desc = b**2-4*a*c; 
+  let x1;
+  let x2; 
+  if (desc > 0) {
+    x1=(-b+Math.sqrt(desc))/(2*a);
+    x2=(-b-Math.sqrt(desc))/(2*a);
+    arr.push(x1,x2);
+    return arr;
+  } 
+  if (desc === 0) {
+    x1=-b/(2*a);
+    arr.push(x1);
+    return arr;
+  } 
   return arr; // array
 }
 
