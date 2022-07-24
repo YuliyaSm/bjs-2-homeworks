10939a0bf4d8bb982dfffae7ee97fb651c9ae50b
Yuliya Smirnova <lavruhinayb@gmail.com>
2022-07-24 20:51:44 +0300

    Homework comleted

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result = false;
+  if (arr1.length === arr2.length) {
+    result = arr1.every((element, index) => element === arr2[index]);
+  }
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  const filteredArr = arr.filter((val) => val > 0 && val % 3 == 0);
+  resultArr = filteredArr.map(val => val * 10);
   return resultArr; // array
 }
